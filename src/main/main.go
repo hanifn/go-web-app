861774c7ea06e6e8419bf248ebcf5e40d40d7930
Hanif Norman <hanifnorman@gmail.com>
2017-01-07 01:21:33 +0800

    start using templates

--- src/main/main.go
+++ src/main/main.go
@@ -2,9 +2,47 @@ package main
 
 import (
     "net/http"
+    "os"
+    "text/template"
 )
 
 func main() {
-    http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+    templates := populateTemplates()
+
+    http.HandleFunc("/",
+        func(w http.ResponseWriter, req *http.Request) {
+            requestedFile := req.URL.Path[1:]
+            template :=
+                templates.Lookup(requestedFile + ".html")
+
+            if template != nil {
+                template.Execute(w, nil)
+            } else {
+                w.WriteHeader(404)
+            }
+        })
+
+    http.ListenAndServe(":8000", nil)
 }
 
+func populateTemplates() *template.Template {
+    result := template.New("templates")
+
+    basePath := "templates"
+    templateFolder, _ := os.Open(basePath)
+    defer templateFolder.Close()
+
+    templatePathsRaw, _ := templateFolder.Readdir(-1)
+
+    templatePaths := new([]string)
+    for _, pathInfo := range templatePathsRaw {
+        if !pathInfo.IsDir() {
+            *templatePaths = append(*templatePaths,
+                basePath + "/" + pathInfo.Name())
+        }
+    }
+
+    result.ParseFiles(*templatePaths...)
+
+    return result
+}
